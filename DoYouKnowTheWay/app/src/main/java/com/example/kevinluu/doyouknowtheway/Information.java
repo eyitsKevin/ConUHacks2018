bb32687734e7fe8066f21b3e08c591ff7311ee17
Michael Tang <michaelnguyentang@Michaels-MacBook-Pro-2.local>
2018-01-28 05:00:19 -0500

    Cleaned MainActivity & 2 & Information & updated UI

--- DoYouKnowTheWay/app/src/main/java/com/example/kevinluu/doyouknowtheway/Information.java
+++ DoYouKnowTheWay/app/src/main/java/com/example/kevinluu/doyouknowtheway/Information.java
@@ -9,34 +9,16 @@ import java.text.DecimalFormat;
 public class Information {
     private String word;
     private String score;
-    private int score2;
-
-    public Information() {
-        this.word = "";
-        this.score = "";
-    }
 
     public Information(String w, String s) {
         this.word = w;
         this.score = s;
     }
 
-    public void setWord(String word) {
-        this.word = word;
-    }
-
-    public void setScore(String score) {
-        this.score = score;
-    }
-
-    public String getWord() {
-        return word;
-    }
-
     public int getScore() {
+
         return (int) (Double.parseDouble(score.replaceAll("\"", ""))*100);
     }
-
 
     public String toString(){
         return "This has a " + getScore() + " percent of being " + word;
